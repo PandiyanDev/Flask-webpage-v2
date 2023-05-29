eccec7019a6d8f74959fa03cb88389f71af544e6
PandiyanDev <107452088+PandiyanDev@users.noreply.github.com>
2023-05-29 04:10:16 +0000

    added dynamic application submitted page

--- database.py
+++ database.py
@@ -36,3 +36,15 @@ def load_job_from_db(id):
     columns = result.keys()
   rows = [row2dict(row, columns) for row in result]
   return None if len(rows) == 0 else rows[0]
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_link, :education, :work_experience, :resume_link)")
+    conn.execute(query,
+                job_id=job_id,
+                full_name=data['full_name'],
+                email=data['email'],
+                linkedin_url = data['linkedin_link'],
+                education=data['education'],
+              work_experience=data['work_experience'],
+                resume_url=data['resume_link'])
